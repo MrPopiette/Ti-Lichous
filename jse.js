010b33fcff6511f3d7ed9794cc67b9a14a258892
Unknown <maxime.monnier@epsi.fr>
2018-06-25 00:48:47 +0200

    MAIL
    Mise en place du formulaire de contact, creation de tilichous.php

--- jse.js
+++ jse.js
@@ -21,17 +21,17 @@ function Step (idName, prevStep, formType, nextStep) {
   this.formType = formType // 0 = Radiobouton  1 = Number  2 = Liste des produits  3 = Validation
   this.nextStep = nextStep
 
-  this.prevStep = function() { //Etape Précédente
-    if(prevStep != 'none') {
-      this.hideBlockCSS();
+  this.prevStep = function () { // Etape Précédente
+    if (prevStep != 'none') {
+      this.hideBlockCSS()
       if (formType == 4) {
-        preValidation.showBlockCSS();
-        activeStep = preValidation;
+        preValidation.showBlockCSS()
+        activeStep = preValidation
       } else {
-        prevStep.showBlockCSS();
-        activeStep = prevStep;
+        prevStep.showBlockCSS()
+        activeStep = prevStep
       }
-      step -= 1;
+      step -= 1
     }
   }
 
@@ -57,29 +57,29 @@ function Step (idName, prevStep, formType, nextStep) {
           alert('Entrez un nombre entre 8 et 60.')
           step -= 1
         }
-      } else if (formType == 2){ //2 = Liste des produits PARTICULIERS
-        insertValuesTab(step, 'Choix de la commande');
-        this.hideBlockCSS();
+      } else if (formType == 2) { // 2 = Liste des produits PARTICULIERS
+        insertValuesTab(step, 'Choix de la commande')
+        this.hideBlockCSS()
         //insérer les valeurs
-        var formValueLength = formValue.length;
-        var quantity = nbPersons;
+        var formValueLength = formValue.length
+        var quantity = nbPersons
         for (var i = 0; i < formValueLength; i++) {
           insertProductTab(i, quantity, formValue[i])
         }
-        console.log(productsTab);
-        preValidation = activeStep;
-        eval(nextStep).showBlockCSS();
-        activeStep = eval(nextStep);
-      } else if (formType == 3){ //3 = Liste des produits PROS
-        insertValuesTab(step, 'Choix de la commande');
-        this.hideBlockCSS();
-        var index = 0;
-        var quantity = nbPersons;
-        insertProductTab(index, quantity, formValue);
-        preValidation = activeStep;
-        eval(nextStep).showBlockCSS();
-        activeStep = eval(nextStep);
-      } else if (formtype == 4){ //Validation
+        console.log(productsTab)
+        preValidation = activeStep
+        eval(nextStep).showBlockCSS()
+        activeStep = eval(nextStep)
+      } else if (formType == 3) { // 3 = Liste des produits PROS
+        insertValuesTab(step, 'Choix de la commande')
+        this.hideBlockCSS()
+        var index = 0
+        var quantity = nbPersons
+        insertProductTab(index, quantity, formValue)
+        preValidation = activeStep
+        eval(nextStep).showBlockCSS()
+        activeStep = eval(nextStep)
+      } else if (formtype == 4) { // Validation
 
       }
       step += 1
@@ -94,34 +94,34 @@ function Step (idName, prevStep, formType, nextStep) {
       inputsLength = inputs.length
       for (var i = 0; i < inputsLength; i++) {
         if (inputs[i].type === 'radio' && inputs[i].checked) {
-          formValue = [i ,inputs[i].value];
+          formValue = [i,inputs[i].value]
         }
       }
-    } else if (formType == 1) { //1 = Number
-      formValue = document.getElementsByName(idName);
-      formValue = formValue[0].value;
-    } else if (formType == 2) { //2 = Liste des produits PARTICULIERS
-      inputs = document.getElementsByName(idName);
-      inputsLength = inputs.length;
-      formValue = new(Array);
+    } else if (formType == 1) { // 1 = Number
+      formValue = document.getElementsByName(idName)
+      formValue = formValue[0].value
+    } else if (formType == 2) { // 2 = Liste des produits PARTICULIERS
+      inputs = document.getElementsByName(idName)
+      inputsLength = inputs.length
+      formValue = new (Array)
       for (var i = 0; i < inputsLength; i++) {
         if (inputs[i].type === 'radio' && inputs[i].checked) {
-          formValue.push(inputs[i].value);
+          formValue.push(inputs[i].value)
         }
         if (inputs[i].type === 'checkbox' && inputs[i].checked) {
-          formValue.push(inputs[i].value);
+          formValue.push(inputs[i].value)
         }
       }
-    } else if (formType == 3) { //3 = Liste des produits PROS (choix unique)
-      inputs = document.getElementsByName(idName);
-      inputsLength = inputs.length;
+    } else if (formType == 3) { // 3 = Liste des produits PROS (choix unique)
+      inputs = document.getElementsByName(idName)
+      inputsLength = inputs.length
       for (var i = 0; i < inputsLength; i++) {
         if (inputs[i].type === 'radio' && inputs[i].checked) {
-          formValue = [inputs[i].value];
+          formValue = [inputs[i].value]
         }
       }
     }
-    return formValue;
+    return formValue
   }
 
   this.showBlockCSS = function () {
@@ -149,17 +149,21 @@ var menusCrepes = new Step('menusCrepes', carteParticulier, 2, nextMenusCrepes)
 var pauseGourmandeSucree = new Step('pauseGourmandeSucree', cartePro, 2, nextPauseGourmandeSucree)
 var pauseGourmandeAperitive = new Step('pauseGourmandeAperitive', cartePro, 2, nextPauseGourmandeAperitive)
 
-var validation = new Step('validation', 'validation', 4, nextValidation);
+var validation = new Step('validation', 'validation', 4, nextValidation)
 
 // VARIABLES
-var erreur = false;
-var step = 0;
-var activeStep = pro;
-var nbPersons;
-var category;
-var preValidation;
-var valuesTab = new(Array);
-var productsTab = new(Array);
+var erreur = false
+var step = 0
+var activeStep = pro
+var nbPersons
+var category
+var preValidation
+var valuesTab = new (Array)
+var productsTab = new (Array)
+var formMail = new (Array)
+var formText = new(Array)
+var formTel = new(Array)
+
 
 //FONCTIONS
 function next () { // Etape suivante
@@ -193,4 +197,16 @@ function displayList () {
       id.style.display = 'none'
     }
   }
+}
+
+function validateForm () {
+ 
+  // var x = document.forms["FormContact"][0 / 1 / 2 selon la position dans le form].value
+  var formMail = document.forms["FormContact"]["fMail"].value
+  var formText = document.forms["FormContact"]["fText"].value
+  var formTel = document.forms["FormContact"]["fTel"].value
+  console.log(formMail)
+  console.log(formText)
+  console.log(formTel)
+  alert("LOL")
 }
